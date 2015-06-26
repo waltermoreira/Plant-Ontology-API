c31b3d0a1a6f327bd2bd11515e7e9a3306240552
Walter Moreira <walter@waltermoreira.net>
2015-06-25 18:47:38 -0700

    Convert return "None" to raising exceptions

--- main.py
+++ main.py
@@ -12,17 +12,15 @@ def search(arg):
     #term = term.upper()
     p = re.compile('[A-Z,a-z,0-9]', re.IGNORECASE)
     if not p.search(term):
-       return
+       raise Exception('"term" is not valid')
 
     r = requests.get('http://palea.cgrb.oregonstate.edu/services/PO_web_service.php?request_type=term_search&search_value=' + term + '&inc_synonyms&branch_filter=plant_anatomy&max=20&prioritize_exact_match')
     
     #print r.headers['Content-Type']
     #print r.text
     
-    if r.ok:
-       return r.headers['Content-Type'], r.content
-    else:
-      return 'text/plaintext; charset=UTF-8', 'An error occurred on the remote server'
+    r.raise_for_status()
+    return r.headers['Content-Type'], r.content
 
 def list(arg):
     pass
